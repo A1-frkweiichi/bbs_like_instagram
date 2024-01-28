ca23222a74adb51d6c723be63140e2880e6f2de5
A1_frkweiichi <engineer.frkw@gmail.com>
2024-01-28 21:20:12 +0900

    Googleログイン機能

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   root "posts#index"
   resources :posts
 
-  devise_for :users
+  devise_for :users, controllers: {
+    omniauth_callbacks: 'users/omniauth_callbacks'
+  }
 
   get "up" => "rails/health#show", as: :rails_health_check
 end
