010ac0acde9b426a04c0a06062f30b8d08fad71f
A1_frkweiichi <engineer.frkw@gmail.com>
2024-01-29 04:06:17 +0900

    deviseユーザー登録できるようにデバッグ
    通常のdeviseユーザー登録で、provider: nil, uid: nilである必要があるため、validationを削除した
    
    irb(main):003> User.all
      User Load (0.3ms)  SELECT "users".* FROM "users" /* loading for pp */ LIMIT ?  [["LIMIT", 11]]
    =>
    [#<User id: 1, email: "eiichi.furukawa@favy.co.jp", created_at: "2024-01-28 18:37:15.527407000 +0000", updated_at: "2024-01-28 18:37:15.527407000 +0000", name: "古川 瑛一", provider: "google_oauth2", uid: "109379701435300316237">,
     #<User id: 2, email: "aaaaa@example.com", created_at: "2024-01-28 18:37:55.911345000 +0000", updated_at: "2024-01-28 18:37:55.911345000 +0000", name: "AAAAA", provider: nil, uid: nil>,
     #<User id: 3, email: "bbb@example.com", created_at: "2024-01-28 18:44:29.426386000 +0000", updated_at: "2024-01-28 18:44:29.426386000 +0000", name: "BBBBB", provider: nil, uid: nil>,
     #<User id: 4, email: "ccc@example.com", created_at: "2024-01-28 18:45:04.850910000 +0000", updated_at: "2024-01-28 18:45:04.850910000 +0000", name: "CCCCC", provider: nil, uid: nil>,
     #<User id: 5, email: "ddd@example.com", created_at: "2024-01-28 19:02:19.244570000 +0000", updated_at: "2024-01-28 19:02:19.244570000 +0000", name: "DDDDD", provider: nil, uid: nil>]

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: %i[google_oauth2]
 
-  validates :uid, uniqueness: { scope: :provider }
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
